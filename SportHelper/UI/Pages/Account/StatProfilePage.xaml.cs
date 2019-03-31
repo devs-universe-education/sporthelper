7c72e41fc85f751284983dc45b3e2bf40ba33333
Helen Menkova <menkova.1998@gmail.com>
2019-03-31 17:35:03 +0300

    Added Account folder

---
+++ SportHelper/UI/Pages/Account/StatProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportHelper.UI.Pages.Account
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StatProfilePage : ContentPage
+	{
+		public StatProfilePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
