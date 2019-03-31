a5690814b0795e58caf5996ea384b313e41e7ed6
Qipap <Qipaps@yandex.ru>
2019-03-31 18:14:25 +0300

    Fixed Account Hierarchy

--- SportHelper/BL/ViewModels/Account/LoginViewModel.cs
+++ SportHelper/BL/ViewModels/Account/LoginViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SportHelper.BL.ViewModels.Account {
-	class LoginViewProfile {
+	class LoginViewModel {
 	}
 }
