5e425ce761a30696f705cf211e790ce53f8fd6c2
Helen Menkova <menkova.1998@gmail.com>
2019-03-31 17:31:19 +0300

    Added Training folder

---
+++ SportHelper/UI/Pages/Training/EditTrainingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportHelper.UI.Pages.Training
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EditTrainingPage : ContentPage
+	{
+		public EditTrainingPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
