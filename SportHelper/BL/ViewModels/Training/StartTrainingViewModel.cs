3e6de8aee729c87e37f2c8dcbe6f06f8864815ec
Qipap <Qipaps@yandex.ru>
2019-03-31 19:30:27 +0300

    New inheritance Training

--- SportHelper/BL/ViewModels/Training/StartTrainingViewModel.cs
+++ SportHelper/BL/ViewModels/Training/StartTrainingViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SportHelper.BL.ViewModels.Account {
-	class StartTrainingViewModel {
+	class StartTrainingViewModel : BaseViewModel {
 	}
 }
