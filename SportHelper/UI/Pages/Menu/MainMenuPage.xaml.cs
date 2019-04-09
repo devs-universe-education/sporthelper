ed5920f87dd7a34b121ebe827e3c35522dc07085
Helen Menkova <menkova.1998@gmail.com>
2019-04-09 18:56:44 +0300

    new inheritance Menu

--- SportHelper/UI/Pages/Menu/MainMenuPage.xaml.cs
+++ SportHelper/UI/Pages/Menu/MainMenuPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace SportHelper.UI.Pages.Menu
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainMenuPage : ContentPage
+	public partial class MainMenuPage : BasePage
 	{
 		public MainMenuPage ()
 		{
