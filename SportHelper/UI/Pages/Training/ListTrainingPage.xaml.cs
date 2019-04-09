d38a9df93e0e4cbd4af421555ba48fac620f94ea
Helen Menkova <menkova.1998@gmail.com>
2019-04-09 18:56:08 +0300

    new inheritance Training

--- SportHelper/UI/Pages/Training/ListTrainingPage.xaml.cs
+++ SportHelper/UI/Pages/Training/ListTrainingPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ using Xamarin.Forms.Xaml;
 
 namespace SportHelper.UI.Pages.Training {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ListTrainingPage : ContentPage {
+	public partial class ListTrainingPage : BasePage {
 		public ListTrainingPage() {
 			InitializeComponent();
 		}
