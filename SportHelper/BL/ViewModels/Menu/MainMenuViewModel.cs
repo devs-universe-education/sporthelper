e56fd662168ee92bc6e5789e66b2519a06a7821a
Qipap <Qipaps@yandex.ru>
2019-04-14 18:24:09 +0300

    Navigation Menu

--- SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
+++ SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
@@ -1,8 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SportHelper.DAL.DataObjects;
+using SportHelper.DAL.DataServices;
 
 namespace SportHelper.BL.ViewModels.Menu {
 	class MainMenuViewModel : BaseViewModel {
+
+		public ICommand GoToListTraining => MakeCommand(GoToListTrainingExecute);
+		public ICommand GoToViewProfile => MakeCommand(GoToViewProfileExecute);
+		public ICommand GoToAboutProgram => MakeCommand(GoToAboutProgramExecute);
+		public ICommand GoToLogin => MakeCommand(GoToLoginExecute);
+
+		void GoToListTrainingExecute() {
+			NavigateTo(AppPages.ListTraining);
+		}
+
+		void GoToViewProfileExecute() {
+			NavigateTo(AppPages.ViewProfile);
+		}
+
+		void GoToAboutProgramExecute() {
+			NavigateTo(AppPages.AboutProgram);
+		}
+
+		void GoToLoginExecute() {
+			NavigateTo(AppPages.Login);
+		}
+
 	}
 }
