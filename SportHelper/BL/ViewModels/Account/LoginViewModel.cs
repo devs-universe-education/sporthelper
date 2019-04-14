f84604b4fddd0f4f3631eef11ae443be31426506
Helen Menkova <menkova.1998@gmail.com>
2019-04-14 18:34:40 +0300

    added Account navigation

--- SportHelper/BL/ViewModels/Account/LoginViewModel.cs
+++ SportHelper/BL/ViewModels/Account/LoginViewModel.cs
@@ -1,9 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SportHelper.DAL.DataObjects;
+using SportHelper.DAL.DataServices;
 
 namespace SportHelper.BL.ViewModels.Account {
 	class LoginViewModel : BaseViewModel {
+		public ICommand GoToRegister => MakeCommand(GoToRegisterExecute);
+		public ICommand GoToMainMenu => MakeCommand(GoToMainMenuExecute);
 
+		void GoToRegisterExecute() {
+
+			NavigateTo(AppPages.Register);
+		}
+
+		void GoToMainMenuExecute() {
+
+			NavigateTo(AppPages.MainMenu);
+		}
 	}
 }
