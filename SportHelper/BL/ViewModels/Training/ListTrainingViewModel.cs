4295350130da822e02a9225dc2b4dcd9371cdd57
Qipap <Qipaps@yandex.ru>
2019-04-14 18:24:40 +0300

    Navigation Training

--- SportHelper/BL/ViewModels/Training/ListTrainingViewModel.cs
+++ SportHelper/BL/ViewModels/Training/ListTrainingViewModel.cs
@@ -1,8 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SportHelper.DAL.DataObjects;
+using SportHelper.DAL.DataServices;
 
-namespace SportHelper.BL.ViewModels.Account {
+namespace SportHelper.BL.ViewModels.Training {
 	class ListTrainingViewModel : BaseViewModel {
+
+
+		public ICommand GoToEditTraining => MakeCommand(GoToEditTrainingExecute);
+		public ICommand GoToStartTraining => MakeCommand(GoToStartTrainingExecute);
+
+		void GoToEditTrainingExecute() {
+			NavigateTo(AppPages.EditTraining);
+		}
+
+		void GoToStartTrainingExecute() {
+			NavigateTo(AppPages.StartTraining);
+		}
 	}
 }
