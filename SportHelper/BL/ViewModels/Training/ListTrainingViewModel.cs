f1a27b1e73c56d0717e0f04850d02764c8688f5c
Qipap <Qipaps@yandex.ru>
2019-05-15 22:47:06 +0300

    Interface Training

--- SportHelper/BL/ViewModels/Training/ListTrainingViewModel.cs
+++ SportHelper/BL/ViewModels/Training/ListTrainingViewModel.cs
@@ -1,14 +1,42 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using SportHelper.DAL.DataObjects;
 using SportHelper.DAL.DataServices;
+using Xamarin.Forms;
+using static SportHelper.BL.ViewModels.Training.Item;
 
 namespace SportHelper.BL.ViewModels.Training {
-	class ListTrainingViewModel : BaseViewModel {
+	public class Item {
+		public Item(string name) {
+			Name = name;
+		}
 
+		public class ItemHandler {
+			public ItemHandler() {
+				Items = new ObservableCollection<Item>();
+			}
+
+			public ObservableCollection<Item> Items { get; private set; }
+
+			public void Add(Item item) {
+				Items.Add(item);
+			}
+		}
+
+		public string Name { get; set; }
+	}
+
+	public class ListTrainingViewModel : BaseViewModel {
+
+		private readonly ItemHandler _itemHandler = new ItemHandler();
+		int i = 0;
 
 		public ICommand GoToEditTraining => MakeCommand(GoToEditTrainingExecute);
 		public ICommand GoToStartTraining => MakeCommand(GoToStartTrainingExecute);
+		public ICommand Test => MakeCommand(TestExecute);
+		public ICommand SampleCommand => MakeCommand(OnSampleCommand);
 
 		void GoToEditTrainingExecute() {
 			NavigateTo(AppPages.EditTraining);
@@ -16,6 +44,19 @@ namespace SportHelper.BL.ViewModels.Training {
 
 		void GoToStartTrainingExecute() {
 			NavigateTo(AppPages.StartTraining);
+		}
+
+		void TestExecute() {
+			_itemHandler.Add(new Item(i.ToString()));
+			i++;
+		}
+
+		public ObservableCollection<Item> Items {
+			get { return _itemHandler.Items; }
+		}
+
+		async void OnSampleCommand() {
+			await ShowAlert("", Application.Current.MainPage.Width.ToString() + " " + Application.Current.MainPage.Height.ToString(), "OK");
 		}
 	}
 }
