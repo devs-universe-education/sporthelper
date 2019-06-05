18f48be6b1e647b49413e95783568b93ed27ab33
Qipap <Qipaps@yandex.ru>
2019-06-05 21:12:59 +0300

    Update Menu

--- SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
+++ SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
@@ -1,15 +1,25 @@
 using System.Threading.Tasks;
 using System.Windows.Input;
+using SportHelper.BL.DB;
+using SportHelper.BL.Model;
 using SportHelper.DAL.DataObjects;
 using SportHelper.DAL.DataServices;
 
 namespace SportHelper.BL.ViewModels.Menu {
 	class MainMenuViewModel : BaseViewModel {
 
+		DataBaseConnection a = new DataBaseConnection();
+
 		public ICommand GoToListTraining => MakeCommand(GoToListTrainingExecute);
 		public ICommand GoToViewProfile => MakeCommand(GoToViewProfileExecute);
 		public ICommand GoToAboutProgram => MakeCommand(GoToAboutProgramExecute);
 		public ICommand GoToLogin => MakeCommand(GoToLoginExecute);
+
+		public MainMenuViewModel() {
+
+				
+
+		}
 
 		void GoToListTrainingExecute() {
 			NavigateTo(AppPages.ListTraining);
@@ -24,6 +34,8 @@ namespace SportHelper.BL.ViewModels.Menu {
 		}
 
 		void GoToLoginExecute() {
+
+			a.db.Execute("UPDATE CurrentUserTable SET Remember = '0' WHERE id_user = 1");
 			NavigateTo(AppPages.Login);
 		}
 
