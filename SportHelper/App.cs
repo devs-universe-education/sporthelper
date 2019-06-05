211dcb96a3e14e815230ae69d78fa28bba802480
Qipap <Qipaps@yandex.ru>
2019-06-05 21:12:14 +0300

    Change first run

--- SportHelper/App.cs
+++ SportHelper/App.cs
@@ -1,3 +1,5 @@
+using SportHelper.BL.DB;
+using SportHelper.BL.Model;
 using SportHelper.DAL.DataServices;
 using SportHelper.UI;
 using Xamarin.Forms;
@@ -8,8 +10,25 @@ namespace SportHelper
 {
 	public class App : Application
 	{
+		DataBaseConnection a = new DataBaseConnection();
+
+		void GetPrepareBD() {
+			a.db.CreateTable<AccountTable>();
+			a.db.CreateTable<TrainingTable>();
+			a.db.CreateTable<ExerciseTable>();
+			a.db.CreateTable<StatisticTable>();
+			a.db.CreateTable<CurrentUserTable>();
+
+			var check = a.db.Query<CurrentUserTable>("Select * From CurrentUserTable");
+			if (check.Count == 0) {
+				a.db.Execute("INSERT INTO CurrentUserTable (id_account) VALUES (0)");
+			}
+		}
+
+
 		public App ()
 		{
+			GetPrepareBD();
 			DialogService.Init(this);
 			NavigationService.Init(this);
 			DataServices.Init(true);
@@ -17,7 +36,9 @@ namespace SportHelper
 
 		protected override void OnStart ()
 		{
-			NavigationService.Instance.SetMainPage(AppPages.MainMenu);
+			
+
+			NavigationService.Instance.SetMainPage(AppPages.Login);
 		}
 	}
 }
