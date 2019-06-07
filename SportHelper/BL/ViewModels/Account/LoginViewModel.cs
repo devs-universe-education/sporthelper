c2a1ab403e9173b84e3c35f41a2e8f3d9c7ee979
Qipap <Qipaps@yandex.ru>
2019-06-07 15:55:56 +0300

    Update Account

--- SportHelper/BL/ViewModels/Account/LoginViewModel.cs
+++ SportHelper/BL/ViewModels/Account/LoginViewModel.cs
@@ -1,21 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using SportHelper.BL.DB;
-using SportHelper.BL.Model;
-using SportHelper.DAL.DataObjects;
 using SportHelper.DAL.DataServices;
+using Xamarin.Forms;
 
 namespace SportHelper.BL.ViewModels.Account {
 	class LoginViewModel : BaseViewModel {
-
-		DataBaseConnection _dataBase = new DataBaseConnection();
-
+		
 		public ICommand GoToRegister => MakeCommand(GoToRegisterExecute);
 		public ICommand GoToMainMenu => MakeCommand(GoToMainMenuExecute);
-		public ICommand GetUserAutorisation => MakeCommand(GetUserAutorisationExecute);
+		public ICommand UserAutorisation => new Command(execute: async () => {
+
+			if (!string.IsNullOrEmpty(PasswordAuto) && !string.IsNullOrEmpty(LoginAuto)) {
+				var logins = await DataServices.SportHelperDataService.GetAccountAsync("SELECT * FROM AccountTable Where Login like '" + LoginAuto + "' AND Password = '" + PasswordAuto + "'", CancellationToken);
+				if (logins.Status == DAL.RequestStatus.Ok) {
+					if (logins.Data.Count == 1) {
+						await DataServices.SportHelperDataService.ExecuteAsync("UPDATE CurrentUserTable SET id_account = " + logins.Data[0].Id + " WHERE id_user = 1", CancellationToken);
+						await DataServices.SportHelperDataService.ExecuteAsync("UPDATE CurrentUserTable SET Remember = " + (CheckRemember ? 1 : 0).ToString() + " WHERE id_user = 1", CancellationToken);
+						NavigateTo(AppPages.MainMenu);
+					}
+					else {
+						await ShowAlert("", "Неверно введен Логин или Пароль", "OK");
+					}
+				}
+			}
+			else {
+				await ShowAlert("", "Заполните поля", "OK");
+			}
+		});
 
 		public string LoginAuto {
 			get => Get<string>();
@@ -32,14 +43,12 @@ namespace SportHelper.BL.ViewModels.Account {
 			set => Set(value);
 		}
 
-		public override Task OnPageAppearing() {
-			var check = _dataBase.db.Query<CurrentUserTable>("Select * From CurrentUserTable");
-			if (check.Count == 1) {
-				if (check[0].Remember == true) {
-					NavigateTo(AppPages.MainMenu);
-				}
+		public override async Task OnPageAppearing() {
+			await DataServices.SportHelperDataService.CreateCurrentUserAsync(CancellationToken);
+			var test = await DataServices.SportHelperDataService.GetCurrentUserAsync("Select * From CurrentUserTable", CancellationToken);
+			if(test.Data[0].Remember == true) {
+				NavigateTo(AppPages.MainMenu);
 			}
-			return base.OnPageAppearing();
 		}
 
 
@@ -51,27 +60,6 @@ namespace SportHelper.BL.ViewModels.Account {
 		void GoToMainMenuExecute() {
 
 			NavigateTo(AppPages.MainMenu);
-		}
-
-		void GetUserAutorisationExecute() {
-
-			if (!string.IsNullOrEmpty(PasswordAuto) && !string.IsNullOrEmpty(LoginAuto)) {
-
-				var logins = _dataBase.db.Query<AccountTable>("SELECT * FROM AccountTable Where Login like '" + LoginAuto + "' AND Password = '" + PasswordAuto + "'");
-				if (logins.Count == 1) {
-
-					_dataBase.db.Execute("UPDATE CurrentUserTable SET id_account = " + logins[0].Id + " WHERE id_user = 1");
-					_dataBase.db.Execute("UPDATE CurrentUserTable SET Remember = " + (CheckRemember ? 1 : 0).ToString() + " WHERE id_user = 1");
-					var check = _dataBase.db.Query<CurrentUserTable>("Select * From CurrentUserTable");
-					NavigateTo(AppPages.MainMenu);
-				}
-				else
-					ShowAlert("", "Неверно введен Логин или Пароль", "OK");
-
-			}
-			else {
-				ShowAlert("", "Заполните поля", "OK");
-			}
 		}
 	}
 }
