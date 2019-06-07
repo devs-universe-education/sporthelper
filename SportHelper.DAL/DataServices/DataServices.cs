d087b1ff78699ffab7beba55f5980591e9e8b5fe
Qipap <Qipaps@yandex.ru>
2019-06-07 15:59:44 +0300

    Others

--- SportHelper.DAL/DataServices/DataServices.cs
+++ SportHelper.DAL/DataServices/DataServices.cs
@@ -1,16 +1,19 @@
 using System;
+using SportHelper.DAL.DataServices;
 
 namespace SportHelper.DAL.DataServices
 {
 	public static class DataServices {
 		public static IMainDataService Main { get; private set; }
 
-		public static void Init(bool isMock) {
+		public static ISportHelperService SportHelperDataService {get; private set;}
+
+		public static void Init(bool isMock, string dbPath) {
 			if (isMock) {
-				Main = new Mock.MockMainDataService();
+				SportHelperDataService = new Mock.MockSportHelperDataService();
 			}
 			else {
-				throw new NotImplementedException("Online Data Services not implemented");
+				SportHelperDataService = new DataBaseConnection(dbPath);
 			}
 		}
 	}
