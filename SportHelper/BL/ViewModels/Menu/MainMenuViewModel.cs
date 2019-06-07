8d6988110cef86feace01b21f73a15aaabad762c
Qipap <Qipaps@yandex.ru>
2019-06-07 15:59:00 +0300

    Update MainMenu

--- SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
+++ SportHelper/BL/ViewModels/Menu/MainMenuViewModel.cs
@@ -1,25 +1,17 @@
-using System.Threading.Tasks;
 using System.Windows.Input;
-using SportHelper.BL.DB;
-using SportHelper.BL.Model;
-using SportHelper.DAL.DataObjects;
 using SportHelper.DAL.DataServices;
+using Xamarin.Forms;
 
 namespace SportHelper.BL.ViewModels.Menu {
 	class MainMenuViewModel : BaseViewModel {
 
-		DataBaseConnection a = new DataBaseConnection();
-
 		public ICommand GoToListTraining => MakeCommand(GoToListTrainingExecute);
 		public ICommand GoToViewProfile => MakeCommand(GoToViewProfileExecute);
 		public ICommand GoToAboutProgram => MakeCommand(GoToAboutProgramExecute);
-		public ICommand GoToLogin => MakeCommand(GoToLoginExecute);
-
-		public MainMenuViewModel() {
-
-				
-
-		}
+		public ICommand GoToLogin => new Command(execute: async () => {
+			await DataServices.SportHelperDataService.ExecuteAsync("UPDATE CurrentUserTable SET Remember = '0' WHERE id_user = 1", CancellationToken);
+			NavigateTo(AppPages.Login);
+		});
 
 		void GoToListTrainingExecute() {
 			NavigateTo(AppPages.ListTraining);
@@ -32,12 +24,5 @@ namespace SportHelper.BL.ViewModels.Menu {
 		void GoToAboutProgramExecute() {
 			NavigateTo(AppPages.AboutProgram);
 		}
-
-		void GoToLoginExecute() {
-
-			a.db.Execute("UPDATE CurrentUserTable SET Remember = '0' WHERE id_user = 1");
-			NavigateTo(AppPages.Login);
-		}
-
 	}
 }
